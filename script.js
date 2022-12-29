0bdb76cf256c709dd35c47ae5a100dee99ebfb8c
graymigii <abdullahkassa@gmail.com>
2022-12-29 16:13:34 +0100

    added a way to populate the display

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (func, a, b) => func(a, b);
+
+const populateDisplay = () => {
+    const digits = document.querySelectorAll('.digit');
+    const display = document.querySelector('.display');
+    let displayContent = '';
+    digits.forEach( btn => {
+        btn.addEventListener('click', (e) => {
+            let textContent = e.target.textContent;
+            displayContent += textContent;
+            display.textContent = displayContent;
+        });
+    });
+    
+};
+
+populateDisplay();
